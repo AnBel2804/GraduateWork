ce55dd4c4c0cf61f3f6e3696024db254c8729569
AnBel2804 <anbelov9@gmail.com>
2023-04-19 23:49:25 +0300

    Add Index page for DepartmentController + Add for DepartmentController

--- Graduate Work/Graduate Work/Areas/Moderator/Controllers/DepartmentController.cs
+++ Graduate Work/Graduate Work/Areas/Moderator/Controllers/DepartmentController.cs
@@ -24,5 +24,38 @@ namespace Graduate_Work.Areas.Moderator.Controllers
             var departments = _unitOfWork.Department.GetAll();
             return View(departments);
         }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Department department)
+        {
+            if (string.IsNullOrEmpty(department.City))
+                ModelState.AddModelError(nameof(department.City), "Назва міста неможе бути пустою");
+
+            if (string.IsNullOrEmpty(department.NumberOfDepartment.ToString()))
+                ModelState.AddModelError(nameof(department.NumberOfDepartment), "Номер відділення не може бути пустим");
+
+            if (!string.IsNullOrEmpty(department.NumberOfDepartment.ToString()))
+                if (department.NumberOfDepartment < 1)
+                    ModelState.AddModelError(nameof(department.NumberOfDepartment), "Номер відділення не може бути меншим за 1");
+
+            if (!string.IsNullOrEmpty(department.City))
+            {
+                IEnumerable<Department> departments_in_city = _unitOfWork.Department.GetAll(c => c.City == department.City);
+                if (departments_in_city.Count(c => c.NumberOfDepartment == department.NumberOfDepartment) > 0)
+                    ModelState.AddModelError(nameof(department.NumberOfDepartment), "В цьому місті вже існує таке відділення");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Department.Add(department);
+                _unitOfWork.Save();
+                TempData["success"] = "Нове відділення успішно додано";
+                return RedirectToAction("Index");
+            }
+            return View(department);
+        }
     }
 }
