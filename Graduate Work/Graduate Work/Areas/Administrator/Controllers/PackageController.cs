27beb968927be5d342ba989b23eb0d4f451e1fcb
AnBel2804 <anbelov9@gmail.com>
2023-04-22 19:05:54 +0300

    Add PackageController + Add Index page for PackageController with search

---
+++ Graduate Work/Graduate Work/Areas/Administrator/Controllers/PackageController.cs
@@ -0,0 +1,46 @@
+﻿using Graduate_Work.Models;
+using Graduate_Work.Repository.IRepository;
+using Graduate_Work.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Graduate_Work.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    [Authorize(Roles = Roles.Role_Administrator)]
+    public class PackageController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PackageController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var packages = _unitOfWork.Package.GetAll(null, "PackageType", "SenderInfo", "ReciverInfo");
+            return View(packages);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string? SearchString)
+        {
+            var packages = _unitOfWork.Package.GetAll(null, "PackageType", "SenderInfo", "ReciverInfo");
+            ViewData["SearchString"] = SearchString;
+            var searchPackages = new List<Package>();
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                foreach (var package in packages)
+                { 
+                    if (package.SenderInfo.Sender.User.UserName == SearchString)
+                        searchPackages.Add(package);
+                    if (package.ReciverInfo.Reciver.User.UserName == SearchString)
+                        searchPackages.Add(package);
+                }
+                return View(searchPackages);
+            }
+            return View(packages);
+        }
+    }
+}
