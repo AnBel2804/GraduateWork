aa51204c83e5ca7886808b03f7fd314a77a7039a
AnBel2804 <anbelov9@gmail.com>
2023-04-25 11:44:59 +0300

    Add Update for Customer

--- Graduate Work/Graduate Work/Areas/Customer/Controllers/UserController.cs
+++ Graduate Work/Graduate Work/Areas/Customer/Controllers/UserController.cs
@@ -1,8 +1,10 @@
-﻿using Graduate_Work.Repository.IRepository;
+﻿using Graduate_Work.Models;
+using Graduate_Work.Repository.IRepository;
 using Graduate_Work.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 
 namespace Graduate_Work.Areas.Customer.Controllers
 {
@@ -25,6 +27,55 @@ namespace Graduate_Work.Areas.Customer.Controllers
             var customer = _unitOfWork.Customer.GetFirstOrDefault(c =>
                     c.User.Id == claim.Value, "User");
 
+            return View(customer);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var customerFromDbFirst = _unitOfWork.Customer.GetFirstOrDefault(c => c.CustomerId == id);
+            if (customerFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerFromDbFirst);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Models.Customer customer)
+        {
+            if (string.IsNullOrEmpty(customer.FirstName))
+                ModelState.AddModelError(nameof(customer.FirstName), "Ім`я є пустим");
+
+            if (string.IsNullOrEmpty(customer.LastName))
+                ModelState.AddModelError(nameof(customer.LastName), "Прізвище є пустим");
+
+            if (!string.IsNullOrEmpty(customer.FirstName) && customer.FirstName.Length < 4)
+                ModelState.AddModelError(nameof(customer.FirstName), "Ім`я не може бути коротшим за 4 символи");
+
+            if (!string.IsNullOrEmpty(customer.FirstName) && customer.FirstName.Length > 25)
+                ModelState.AddModelError(nameof(customer.FirstName), "Ім`я не може бути довшим за 25 символів");
+
+            if (!string.IsNullOrEmpty(customer.LastName) && customer.LastName.Length < 4)
+                ModelState.AddModelError(nameof(customer.LastName), "Прізвище не може бути коротшим за 4 символи");
+
+            if (!string.IsNullOrEmpty(customer.LastName) && customer.LastName.Length > 25)
+                ModelState.AddModelError(nameof(customer.LastName), "Прізвище не може бути довшим за 25 символів");
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Customer.Update(customer);
+                _unitOfWork.Save();
+                TempData["success"] = "Інформацію успішно відредаговано";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Інформацію не було відредаговано";
             return View(customer);
         }
     }
--- Graduate Work/Graduate Work/Models/Customer.cs
+++ Graduate Work/Graduate Work/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,8 +11,18 @@ namespace Graduate_Work.Models
     {
         [Key]
         public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Ім`я є пустим")]
+        [StringLength(25, ErrorMessage = "Ім`я не може бути довшим за 25 символів")]
+        [MinLength(4, ErrorMessage = "Ім`я не може бути коротшим за 4 символи")]
+        [DisplayName("Ім`я")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Прізвище є пустим")]
+        [StringLength(25, ErrorMessage = "Прізвище не може бути довшим за 25 символів")]
+        [MinLength(4, ErrorMessage = "Прізвище не може бути коротшим за 4 символи")]
+        [DisplayName("Прізвище")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Номер телефону є пустим")]
+        [DisplayName("Номер телефону")]
         public string Phone { get; set; }
         [ValidateNever]
         [Required]
