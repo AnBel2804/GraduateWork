7e8e8246d3753dfce537eb8f46e843e0b26fcb7b
AnBel2804 <anbelov9@gmail.com>
2023-04-27 17:32:49 +0300

    Edit Customer Area

--- Graduate Work/Graduate Work/Areas/User/Controllers/HomeController.cs
+++ Graduate Work/Graduate Work/Areas/User/Controllers/HomeController.cs
@@ -10,10 +10,38 @@ namespace Graduate_Work.Areas.User.Controllers
     [Area("User")]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchPackage(string? SearchString)
+        {
+            var packages = _unitOfWork.Package.GetAll(null, "PackageType", "SenderInfo", "ReciverInfo");
+            ViewData["SearchString"] = SearchString;
+            var searchPackage = new Package();
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                searchPackage = packages.FirstOrDefault(c => c.TTN == SearchString);
+                if (searchPackage != null)
+                    return View(searchPackage);
+                else
+                {
+                    TempData["error"] = "Посилку не було знайдено";
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Privacy()
         {
             return View();
